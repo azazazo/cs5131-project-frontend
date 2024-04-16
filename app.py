f9b86cc17458c12d6559ccd4851bc3989319344c
Viron <josephziegler19@gmail.com>
2024-04-16 23:34:46 +0800

    Fixed

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ classes = [
     "CWE-469: Use of Pointer Subtraction to Determine Size"
 ]
 
+prediction = None
+result = None
+message = None
+
 app = Flask(__name__)
 
 def softmax(x):
@@ -31,7 +35,13 @@ def index():
 
 @app.route('/detect', methods=["POST"])
 def detect():
-    code = request.form["code"]
+    global prediction
+    global result
+    global message
+
+    json = request.get_json() 
+    code = json['code']
+
     try:
         lexed = lex(code)
         embedded = np.array([embedding.wv[s] for s in lexed])
@@ -44,24 +54,37 @@ def detect():
 
         data = tf.data.Dataset.from_tensor_slices(embedded).batch(1)
 
-        prediction = model.predict(data)[0, :-2]
+        _prediction = model.predict(data)[0, :-2]
     except:
         return render_template('/error.html')
     
-    prediction = softmax(prediction)
-    c = np.argmax(prediction)
+    _prediction = softmax(_prediction)
+    c = np.argmax(_prediction)
     if c == 0:
         result = "safe :)"
     else:
         result = "vulnerable :("
     message = classes[c]
 
-    prediction = {k: v*100 for k, v in zip(["Safe", "CWE-119", "CWE-120", "CWE-469"], prediction)}
-    prediction = dict(sorted(prediction.items(), key=lambda item: -item[1]))
-    print(prediction)
-    prediction = "\n".join([f"{k}: {v:.2f}%" for k, v in prediction.items()])
+    _prediction = {k: v*100 for k, v in zip(["Safe", "CWE-119", "CWE-120", "CWE-469"], _prediction)}
+    _prediction = dict(sorted(_prediction.items(), key=lambda item: -item[1]))
+    print(_prediction)
+    prediction = "\n".join([f"{k}: {v:.2f}%" for k, v in _prediction.items()])
 
+    return {}
+
+
+@app.route('/results', methods=['GET'])
+def results():
+    global prediction
+    global result
+    global message
+
+    print(prediction)
+    print(result)
+    print(message)
     return render_template("/results.html", prediction=prediction, result=result, message=message)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
--- static/form.js
+++ static/form.js
@@ -1,44 +1,52 @@
 // Get the current URL
 const url = window.location.href;
 
-$(document).ready(function() {
-    console.log("Document is ready");
+$("#upload").on("click", function() {
+    $('#file-input').trigger('click');
 });
 
-$("#upload").on("click", function() {
-    alert("Upload button clicked");
-});
+$('#file-input').onchange = e => { 
+    console.log("Hon")
+   var file = e.target.files[0]; 
+
+   var reader = new FileReader();
+   reader.readAsDataURL(file); 
+
+   reader.onload = readerEvent => {
+      var content = readerEvent.target.result; 
+      $("#code").val(content);
+   }
+}
 
 $("#submit").on("click", function() {    
     // Get all form elements
-    alert("Submit button clicked");
     var json = {};
 
     field = $("#code").serializeArray()[0];
 
-    if (field.value.length != 0) {
-        // Add the field to the JSON object
-        json[field.name] = field.value;
-    } else {
-        // Alert the user and return if a field is empty
+    if (field.value.length == 0) {
         alert("Cannot have empty field");
         return false;
     }
+
+    json['code'] = field.value;
 
     console.log(json);
 
     // Send a POST request with the JSON data
     $.ajax({
-        url: url,
+        url: '/detect',
         type: 'POST',
         dataType: 'json',
         contentType: 'application/json',
         data: JSON.stringify(json),
         success: function(data) {
+            window.location.href = '/results'
             // Handle the response here
         },
         error: function(error) {
             console.log(error)
         }
     });
+
 });
